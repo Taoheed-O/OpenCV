7832fa9268cb32de349d8d6d31bcd29a39f10575
Taoheed Oyeniyi <oyeniyiemperor@gmail.com>
2023-04-05 11:11:24 +0100

    successfully showed a video

--- chapter1.py
+++ chapter1.py
@@ -1,6 +1,13 @@
 import cv2
 
-print('package imported')
-img = cv2.imread('resources/ubuntu.png')
-cv2.imshow('output', img)
-cv2.waitKey(4000)
+# print('package imported')
+# img = cv2.imread('resources/images/ubuntu.png')
+# cv2.imshow('output', img)
+# cv2.waitKey(4000)
+
+cap = cv2.VideoCapture('resources/videos/baby.mp4')
+while True:
+    success, img = cap.read()
+    cv2.imshow('video', img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
