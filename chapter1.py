c64b5a9ca938cd6a61641873efdd17eedd031b9b
Taoheed Oyeniyi <oyeniyiemperor@gmail.com>
2023-04-05 14:09:41 +0100

    opened webcam successfully

--- chapter1.py
+++ chapter1.py
@@ -1,13 +1,27 @@
 import cv2
 
+# image
 # print('package imported')
 # img = cv2.imread('resources/images/ubuntu.png')
 # cv2.imshow('output', img)
 # cv2.waitKey(4000)
 
-cap = cv2.VideoCapture('resources/videos/baby.mp4')
+# video
+# cap = cv2.VideoCapture('resources/videos/baby.mp4')
+# while True:
+#     success, img = cap.read()
+#     cv2.imshow('video', img)
+#     if cv2.waitKey(1) & 0xFF == ord('q'):
+#         break
+
+# webcam
+cam = cv2.VideoCapture(0)
+
 while True:
-    success, img = cap.read()
-    cv2.imshow('video', img)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    check, frame = cam.read()
+    cv2.imshow('video', frame)
+    key = cv2.waitKey(1)
+    if key & 0xFF == ord('q'):
         break
+cam.release()
+cv2.destroyAllWindows()
