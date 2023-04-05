a7cbb7629ade05dbe4caf422df7c596235aa270b
Taoheed Oyeniyi <oyeniyiemperor@gmail.com>
2023-04-05 15:29:07 +0100

    different color sections

--- chapter1.py
+++ chapter1.py
@@ -1,5 +1,5 @@
 import cv2
-
+import numpy as np
 # image
 # print('package imported')
 # img = cv2.imread('resources/images/ubuntu.png')
@@ -27,8 +27,18 @@ import cv2
 # cv2.destroyAllWindows()
 
 img =  cv2.imread('resources/images/ubuntu.png')
+kernel = np.ones((5,5), np.uint8)
 
 imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+imgBlur =  cv2.GaussianBlur(imgGray,(7,7), 0)
+imgCanny = cv2.Canny(img, 100, 100)
+imgDialation = cv2.dilate(imgCanny, kernel, iterations=1)
+imgEroded = cv2.erode(imgDialation, kernel, iterations=1)
 
 cv2.imshow('gray', imgGray)
+cv2.imshow('blur', imgBlur)
+cv2.imshow('canny', imgCanny)
+cv2.imshow('Dilation', imgDialation)
+cv2.imshow('Eroded', imgEroded)
+
 cv2.waitKey(0)
