17be9b9df0f672e455ca7cad260c092378f06826
Taoheed Oyeniyi <oyeniyiemperor@gmail.com>
2023-04-06 15:12:30 +0100

    joining images

--- chapter1.py
+++ chapter1.py
@@ -78,3 +78,15 @@ import numpy as np
 
 
 # Warp perspective
+img = cv2.imread('resources/images/linux_mint_logo.png')
+
+width, height = 250,350
+pts1 = np.float32([[111, 219], [287, 188], [154, 482],[352, 440]])
+pts2 = np.float32([[0,0],[width,0],[0,height],[width, height]])
+matrix = cv2.getPerspectiveTransform(pts1, pts2)
+imgOutput = cv2.warpPerspective(img, matrix, (width, height))
+
+cv2.imshow('Image', img)
+cv2.imshow('output', imgOutput)
+
+cv2.waitKey(0)
