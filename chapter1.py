65e2a4006e96483ba68f17ca082813555093d65f
Taoheed Oyeniyi <oyeniyiemperor@gmail.com>
2023-04-06 14:32:19 +0100

    warp pespective

--- chapter1.py
+++ chapter1.py
@@ -45,16 +45,36 @@ import numpy as np
 # cv2.waitKey(0)
 
 # Resize and cropping
-img = cv2.imread('resources/images/linux_mint_logo.png')
-print(img.shape)
+# img = cv2.imread('resources/images/linux_mint_logo.png')
+# print(img.shape)
 
-imgResize = cv2.resize(img, (300, 200))
-print(imgResize.shape)
+# imgResize = cv2.resize(img, (300, 200))
+# print(imgResize.shape)
 
-imgCropped = img[0:200, 200:500]
+# imgCropped = img[0:200, 200:500]
 
-cv2.imshow('Image', img)
-cv2.imshow('Image Resize', imgResize)
-cv2.imshow('Image crop',imgCropped)
+# cv2.imshow('Image', img)
+# cv2.imshow('Image Resize', imgResize)
+# cv2.imshow('Image crop',imgCropped)
 
-cv2.waitKey(0)
+# cv2.waitKey(0)
+
+
+# drawing on images
+
+# img = np.zeros((512, 512,3), np.uint8)
+
+# print(img.shape)
+# img[200:500, 100:300] = 255,0,0
+
+# cv2.line(img, (0,0), (img.shape[1],img.shape[0]), (0,255,0), 3)
+# cv2.rectangle(img, (0,0), (250, 350),(0,0, 255), 2) #cv2.FILLED)
+# cv2.circle(img, (400, 50),30, (255, 255, 0), 5)
+# cv2.putText(img, "OpenCV", (300, 200),cv2.FONT_HERSHEY_COMPLEX,1.5,(0,150,0),3)
+
+# cv2.imshow('Image', img)
+
+# cv2.waitKey(0)
+
+
+# Warp perspective
