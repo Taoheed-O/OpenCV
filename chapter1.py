452a1db443265f882f79e9fc288f23a4883a195a
Taoheed Oyeniyi <oyeniyiemperor@gmail.com>
2023-04-08 16:21:08 +0100

    color detection

--- chapter1.py
+++ chapter1.py
@@ -124,7 +124,7 @@ import numpy as np
 # color detection
 
 
-def func():
+def func(a):
     pass
 
 
@@ -133,24 +133,35 @@ cv2.namedWindow('TrackBars')
 cv2.resizeWindow('TrackBars', 640, 240)
 
 
-cv2.createTrackbar('Hue Max', 'TrackBars', 179,179, func)
+cv2.createTrackbar('Hue Max', 'TrackBars', 4,179, func)
 cv2.createTrackbar('Hue Min', 'TrackBars', 0,179, func)
 cv2.createTrackbar('Sat Max', 'TrackBars', 255,255, func)
 cv2.createTrackbar('Sat Min', 'TrackBars', 0,255, func)
 cv2.createTrackbar('Val Max', 'TrackBars', 255,255, func)
-cv2.createTrackbar('Val Min', 'TrackBars', 0,255, func)
-
-
-img = cv2.imread(path)
+cv2.createTrackbar('Val Min', 'TrackBars', 7,255, func)
 
 
 
-imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+while True:
+    img = cv2.imread(path)
+    imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    h_max = cv2.getTrackbarPos('Hue Max', 'TrackBars')
+    h_min = cv2.getTrackbarPos('Hue Min', 'TrackBars')
+    sat_max = cv2.getTrackbarPos('Sat Max', 'TrackBars')
+    sat_min = cv2.getTrackbarPos('Sat Min', 'TrackBars')
+    val_max = cv2.getTrackbarPos('Val Max', 'TrackBars')
+    val_min = cv2.getTrackbarPos('Val Min', 'TrackBars')
 
+    print(h_max, h_min, sat_max, sat_min, val_max, val_min)
+    lower = np.array([h_min, sat_min, val_min])
+    upper = np.array([h_max, sat_max, val_max])
+    mask  = cv2.inRange(imgHSV, lower, upper)
 
+    imgResult = cv2.bitwise_and(img,img, mask=mask)
 
-cv2.imshow('original', img)
-cv2.imshow('hsv',imgHSV)
-
-
-cv2.waitKey(0)
+    # show
+    cv2.imshow('original', img)
+    cv2.imshow('hsv',imgHSV)
+    cv2.imshow('mask', mask)
+    cv2.imshow('img Result', imgResult)
+    cv2.waitKey(1)
