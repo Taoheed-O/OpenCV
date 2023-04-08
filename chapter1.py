48e0c431f7401aca53b9caf6dbc09fa7131e1462
Taoheed Oyeniyi <oyeniyiemperor@gmail.com>
2023-04-08 14:38:24 +0100

    color detection

--- chapter1.py
+++ chapter1.py
@@ -2,17 +2,19 @@ import cv2
 import numpy as np
 
 
-def stackImages(scale, imgArray):
-    rows = len(imgArray)
-    cols = len(imgArray[0])
-    rowsAvailable = isinstance(imgArray[0], list)
-    width = imgArray[0][0].shape[1]
-    height = imgArray[0][0].shape[0]
-    if rowsAvailable:
-        for x in range(0, rows):
-            for y in range(0, cols):
-                if imgArray[x][y].shape[:2] == imgArray[0][0].shape[:2]:
-                    imgArray[x][y] = cv2.resize(imgArray[x][y], (0,0), None,)   
+# incomplete function...
+# def stackImages(scale, imgArray):
+#     rows = len(imgArray)
+#     cols = len(imgArray[0])
+#     rowsAvailable = isinstance(imgArray[0], list)
+#     width = imgArray[0][0].shape[1]
+#     height = imgArray[0][0].shape[0]
+#     if rowsAvailable:
+#         for x in range(0, rows):
+#             for y in range(0, cols):
+#                 if imgArray[x][y].shape[:2] == imgArray[0][0].shape[:2]:
+#                     imgArray[x][y] = cv2.resize(imgArray[x][y], (0,0), None,)   
+
 
 
 # image
@@ -108,7 +110,7 @@ def stackImages(scale, imgArray):
 
 
 # joining Images
-img = cv2.imread('resources/images/linux_mint_logo.png')
+# img = cv2.imread('resources/images/linux_mint_logo.png')
 
 # imgHor = np.hstack((img, img))
 # imgVert = np.vstack((img, img))
@@ -116,4 +118,13 @@ img = cv2.imread('resources/images/linux_mint_logo.png')
 # cv2.imshow('horizontal stack', imgHor)
 # cv2.imshow('vertical stack', imgVert)
 
+# cv2.waitKey(0)
+
+
+# color detection
+
+path = 'resources/images/linux_mint_logo.png'
+img = cv2.imread(path)
+
+cv2.imshow('original', img)
 cv2.waitKey(0)
