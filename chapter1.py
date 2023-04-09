ecb4f5baa13af0bcdc58566b16ac06887d785938
Taoheed Oyeniyi <oyeniyiemperor@gmail.com>
2023-04-09 17:24:29 +0100

    contour and shape detection

--- chapter1.py
+++ chapter1.py
@@ -122,46 +122,59 @@ import numpy as np
 
 
 # color detection
+# def func(a):
+#     pass
 
 
-def func(a):
-    pass
+# path = 'resources/images/linux_mint_logo.png'
+# cv2.namedWindow('TrackBars')
+# cv2.resizeWindow('TrackBars', 640, 240)
 
 
-path = 'resources/images/linux_mint_logo.png'
-cv2.namedWindow('TrackBars')
-cv2.resizeWindow('TrackBars', 640, 240)
-
-
-cv2.createTrackbar('Hue Max', 'TrackBars', 4,179, func)
-cv2.createTrackbar('Hue Min', 'TrackBars', 0,179, func)
-cv2.createTrackbar('Sat Max', 'TrackBars', 255,255, func)
-cv2.createTrackbar('Sat Min', 'TrackBars', 0,255, func)
-cv2.createTrackbar('Val Max', 'TrackBars', 255,255, func)
-cv2.createTrackbar('Val Min', 'TrackBars', 7,255, func)
+# cv2.createTrackbar('Hue Max', 'TrackBars', 4,179, func)
+# cv2.createTrackbar('Hue Min', 'TrackBars', 0,179, func)
+# cv2.createTrackbar('Sat Max', 'TrackBars', 255,255, func)
+# cv2.createTrackbar('Sat Min', 'TrackBars', 0,255, func)
+# cv2.createTrackbar('Val Max', 'TrackBars', 255,255, func)
+# cv2.createTrackbar('Val Min', 'TrackBars', 7,255, func)
 
 
 
-while True:
-    img = cv2.imread(path)
-    imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    h_max = cv2.getTrackbarPos('Hue Max', 'TrackBars')
-    h_min = cv2.getTrackbarPos('Hue Min', 'TrackBars')
-    sat_max = cv2.getTrackbarPos('Sat Max', 'TrackBars')
-    sat_min = cv2.getTrackbarPos('Sat Min', 'TrackBars')
-    val_max = cv2.getTrackbarPos('Val Max', 'TrackBars')
-    val_min = cv2.getTrackbarPos('Val Min', 'TrackBars')
+# while True:
+#     img = cv2.imread(path)
+#     imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+#     h_max = cv2.getTrackbarPos('Hue Max', 'TrackBars')
+#     h_min = cv2.getTrackbarPos('Hue Min', 'TrackBars')
+#     sat_max = cv2.getTrackbarPos('Sat Max', 'TrackBars')
+#     sat_min = cv2.getTrackbarPos('Sat Min', 'TrackBars')
+#     val_max = cv2.getTrackbarPos('Val Max', 'TrackBars')
+#     val_min = cv2.getTrackbarPos('Val Min', 'TrackBars')
 
-    print(h_max, h_min, sat_max, sat_min, val_max, val_min)
-    lower = np.array([h_min, sat_min, val_min])
-    upper = np.array([h_max, sat_max, val_max])
-    mask  = cv2.inRange(imgHSV, lower, upper)
+#     print(h_max, h_min, sat_max, sat_min, val_max, val_min)
+#     lower = np.array([h_min, sat_min, val_min])
+#     upper = np.array([h_max, sat_max, val_max])
+#     mask  = cv2.inRange(imgHSV, lower, upper)
 
-    imgResult = cv2.bitwise_and(img,img, mask=mask)
+#     imgResult = cv2.bitwise_and(img,img, mask=mask)
 
-    # show
-    cv2.imshow('original', img)
-    cv2.imshow('hsv',imgHSV)
-    cv2.imshow('mask', mask)
-    cv2.imshow('img Result', imgResult)
-    cv2.waitKey(1)
+#     # show
+#     cv2.imshow('original', img)
+#     cv2.imshow('hsv',imgHSV)
+#     cv2.imshow('mask', mask)
+#     cv2.imshow('img Result', imgResult)
+#     cv2.waitKey(1)
+
+
+# contours/ shape detection
+
+path = 'resources/images/ubuntu.png'
+img = cv2.imread(path)
+
+imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+imgBlur = cv2.GaussianBlur(imgGray, (7,7),1)
+
+
+cv2.imshow('original', img)
+cv2.imshow('Gray', img)
+cv2.imshow('Blur', imgBlur)
+cv2.waitKey(0)
