3f4210e1295f8980edff4283afb5b3e6caac041a
Taoheed Oyeniyi <oyeniyiemperor@gmail.com>
2023-04-11 11:28:46 +0100

    shape detection

--- chapter1.py
+++ chapter1.py
@@ -178,7 +178,33 @@ def getContours(img):
     for cnt in contours:
         area = cv2.contourArea(cnt)
         print(area)
-        cv2.drawContours(imgContour, cnt, -1, (255,0,0),3)
+        if area > 500:
+            cv2.drawContours(imgContour, cnt, -1, (255,0,0),3)
+            peri = cv2.arcLength(cnt, True)
+            print(peri)
+            approx = cv2.approxPolyDP(cnt, 0.02*peri, True)
+            print(len(approx))
+            objCor = len(approx)
+            x, y, w, h = cv2.boundingRect(approx)
+
+            if objCor == 3:
+                objectType = 'Triangle'
+            elif objCor == 4:
+                aspRatio = w/float(h)
+                if aspRatio > 0.95 and aspRatio < 1.05:
+                    objectType = 'square'
+                else:
+                    objectType = 'rectangle'
+            elif objCor > 4:
+                objectType = 'circle'
+            else:
+                objectType = 'None'
+            
+            cv2.rectangle(imgContour, (x,y),(x+w, y+h),(0,255,0), 2)
+            cv2.putText(imgContour, objectType, (x+(w//2)-10, y+(h//2)-10), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0,0,0), 2)
+
+
+
 
 
 
