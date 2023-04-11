be620139315b3bd1802cb69c2058614cd0545860
Taoheed Oyeniyi <oyeniyiemperor@gmail.com>
2023-04-11 14:46:31 +0100

    virtual paint - project 1

--- chapter1.py
+++ chapter1.py
@@ -246,15 +246,19 @@ import numpy as np
 # face detection
 # viola and jones method
 
-faceCascade = cv2.CascadeClassifier('resources/haarcascade_frontalface_default.xml')
-img = cv2.imread('resources/profile_pic.jpeg')
-imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# faceCascade = cv2.CascadeClassifier('resources/files/haarcascade_frontalface_default.xml')
+# img = cv2.imread('resources/images/profile_pic.jpeg')
+# imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 
-faces = faceCascade.detectMultiScale(imgGray, 1.1, 4)
+# faces = faceCascade.detectMultiScale(imgGray, 1.1, 4)
+
+# for (x, y, w, h) in faces:
+#     cv2.rectangle(img, (x,y), (x+w, y+h), (0,0, 255),2)
 
 
 
 
-cv2.imshow('result', img)
-cv2.waitKey(0)
+
+# cv2.imshow('result', img)
+# cv2.waitKey(0)
