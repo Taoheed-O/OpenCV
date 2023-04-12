eaa28f303edde65832043155ae9cc4ab8055f04d
Taoheed Oyeniyi <oyeniyiemperor@gmail.com>
2023-04-12 12:21:24 +0100

    added a file color detection .py

--- virtual_paint.py
+++ virtual_paint.py
@@ -1,4 +1,5 @@
 import cv2 
+import numpy as np
 
 # webcam
 
@@ -6,6 +7,7 @@ import cv2
 frameWidth = 640
 frameHeight = 480
 
+# camera initialization
 cam = cv2.VideoCapture(0)
 
 # cam settings
@@ -13,6 +15,22 @@ cam.set(3, frameWidth)
 cam.set(4, frameHeight)
 cam.set(10, 150)
 
+
+# color list
+myColors = []
+
+
+# color finding function 
+# def find_color(img):
+#     imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+#     lower = np.array([h_min, sat_min, val_min])
+#     upper = np.array([h_max, sat_max, val_max])
+#     mask  = cv2.inRange(imgHSV, lower, upper)
+#     cv2.imshow('img', mask)
+
+
+
+# cam view loop
 while True:
     check, frame = cam.read()
     cv2.imshow('video', frame)
