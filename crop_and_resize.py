00f16a93414fd30a29526085789400c7ff2bee08
Taoheed Oyeniyi <oyeniyiemperor@gmail.com>
2023-04-17 12:32:26 +0100

    code review

--- crop_and_resize.py
+++ crop_and_resize.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-img = cv2.imread("Resources/shapes.png")
+img = cv2.imread("resources/images/profile_pic.jpeg")
 print(img.shape)
 
 imgResize = cv2.resize(img,(1000,500))
