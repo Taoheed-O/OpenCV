5dec1cb567a8f590a585f7ed8f2116c3065fd4f7
Taoheed Oyeniyi <oyeniyiemperor@gmail.com>
2023-04-17 12:33:05 +0100

    code review

--- image_joining.py
+++ image_joining.py
@@ -33,7 +33,7 @@ def stackImages(scale,imgArray):
         ver = hor
     return ver
 
-img = cv2.imread('Resources/lena.png')
+img = cv2.imread('resources/images/profile_pic.jpeg')
 imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
 imgStack = stackImages(0.5,([img,imgGray,img],[img,img,img]))
