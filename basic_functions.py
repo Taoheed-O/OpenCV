339d4a0b2cf9dca1179809c27cd3a8eede324c58
Taoheed Oyeniyi <oyeniyiemperor@gmail.com>
2023-04-17 12:22:13 +0100

    code review

--- basic_functions.py
+++ basic_functions.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
  
-img = cv2.imread("Resources/lena.png")
+img = cv2.imread("resources/images/profile_pic.jpeg")
 kernel = np.ones((5,5),np.uint8)
  
 imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
