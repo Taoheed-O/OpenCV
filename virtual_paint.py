e726e8b1f46f965e37e28ec6045922361753ecad
Taoheed Oyeniyi <oyeniyiemperor@gmail.com>
2023-04-17 13:21:25 +0100

    virtual paint... color detection

--- virtual_paint.py
+++ virtual_paint.py
@@ -17,22 +17,25 @@ cam.set(10, 150)
 
 
 # color list
-myColors = []
+myColors = [[98, 126, 125, 255, 153, 255],
+            [35, 179, 57, 255, 0, 141]]
 
 
 # color finding function 
-def find_color(img):
+def find_color(img, myColors):
     imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    lower = np.array([h_min, sat_min, val_min])
-    upper = np.array([h_max, sat_max, val_max])
-    mask  = cv2.inRange(imgHSV, lower, upper)
-    cv2.imshow('img', mask)
+    for color in myColors:
+        lower = np.array(myColors[0][0:3])
+        upper = np.array(myColors[0][3:6])
+        mask  = cv2.inRange(imgHSV, lower, upper)
+        cv2.imshow('img', mask)
 
 
 
 # cam view loop
 while True:
     check, frame = cam.read()
+    find_color(frame, myColors)
     cv2.imshow('video', frame)
     key = cv2.waitKey(1)
     if key & 0xFF == ord('q'):
