1cc48912df59374df86b9c8ffd09a9a2ee94fd35
Taoheed Oyeniyi <oyeniyiemperor@gmail.com>
2023-04-19 17:51:01 +0100

    code review

--- document_scanner.py
+++ document_scanner.py
@@ -21,6 +21,8 @@ cam.set(10, 150)
 kernel = np.ones((5,5))
 
 
+
+# image preprocessing function
 def preProcessing(img):
     imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     imgBlur = cv2.GaussianBlur(imgGray, (5,5), 1)
@@ -32,12 +34,33 @@ def preProcessing(img):
 
 
 
+# get contour function
+def getContours(img):
+    biggest = np.array([])
+    maxArea = 0
+    contours,hierarchy = cv2.findContours(img,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
+    for cnt in contours:
+        area = cv2.contourArea(cnt)
+        if area>5000:
+            cv2.drawContours(imgContour, cnt, -1, (255, 0, 0), 3)
+            peri = cv2.arcLength(cnt,True)
+            #print(peri)
+            approx = cv2.approxPolyDP(cnt,0.02*peri,True)
+            if (area > maxArea) and (len(approx) == 4):
+                biggest = approx
+                maxArea = area
+
+    return biggest
+
+
 # cam view loop
 while True:
     check, img = cam.read()
     img = cv2.resize(img, (widthImg, heightImg))
+    imgContour = img.copy()
     imgThres = preProcessing(img)
-    cv2.imshow('img', imgThres)
+    getContours(imgThres)
+    cv2.imshow('img', imgContour)
     key = cv2.waitKey(1)
     if key & 0xFF == ord('q'):
         break
