4d4c2b803dc339805c973b7d1feded2f23577d95
Taoheed Oyeniyi <oyeniyiemperor@gmail.com>
2023-04-26 16:01:25 +0100

    code review

--- plate_number_detector.py
+++ plate_number_detector.py
@@ -5,14 +5,14 @@ import cv2
 #########################
 frameWidth = 640
 frameHeight = 480
-nPlateCascade = cv2.CascadeClassifier("Resources/haarcascade_russian_plate_number.xml")
+nPlateCascade = cv2.CascadeClassifier("resources/haarcascade_files/haarcascade_russian_plate_number.xml")
 minArea = 200
 color = (255,0,255)
 ###############################################
 
 
 
-cap = cv2.VideoCapture("Resources/video12.mp4")
+cap = cv2.VideoCapture("resources/videos/baby.mp4")
 cap.set(3, frameWidth)
 cap.set(4, frameHeight)
 cap.set(10,150)
@@ -28,7 +28,7 @@ while True:
         area = w*h
         if area > minArea:
             cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 255), 2)
-            cv2.putText(img,"Number Plate",(x,y-5),
+            cv2.putText(img,"plate  number",(x,y-5),
                         cv2.FONT_HERSHEY_COMPLEX_SMALL,1,color,2)
             imgRoi = img[y:y+h,x:x+w]
             cv2.imshow("ROI", imgRoi)
@@ -37,7 +37,7 @@ while True:
 
 
     if cv2.waitKey(0) & 0xFF == ord('q'):
-        cv2.imwrite("Resources/Scanned/NoPlate_"+str(count)+".jpg",imgRoi)
+        cv2.imwrite("resources/scanned_plates/NoPlate_"+str(count)+".jpg",imgRoi)
         cv2.rectangle(img,(0,200),(640,300),(0,255,0),cv2.FILLED)
         cv2.putText(img,"Scan Saved",(150,265),cv2.FONT_HERSHEY_DUPLEX,
                     2,(0,0,255),2)
