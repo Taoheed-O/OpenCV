7345facc6d3f3540098b84843f814cc005ee0b2e
Taoheed Oyeniyi <oyeniyiemperor@gmail.com>
2023-06-24 19:43:28 +0100

    code review

---
+++ .idea/test.py
@@ -0,0 +1,10 @@
+first_name = 'faatimah'
+last_name = 'olofintuyi'
+age = 18
+dept = 'forestry'
+height = 3.75
+fac = 'renewable natural resources'
+is_a_muslimah = True
+is_she_good = False
+
+print('hi, my name is' + first_name + last_name + '.\n I am ' + str(age) + 'years old.')
